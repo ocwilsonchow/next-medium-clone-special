5e32e444ab2df316a3eb87a55631b3fcd5062e92
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-16 02:16:53 +0800

    add create chatroom

--- components/chat/ChatRoomList.jsx
+++ components/chat/ChatRoomList.jsx
@@ -1,20 +1,36 @@
-import { Box, Fade, Flex, LinkBox, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Fade,
+  Flex,
+  LinkBox,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import React from "react";
 
 const ChatRoomList = () => {
   return (
-    <Box p={4}>
-      <Text fontSize="lg" fontWeight="medium">
-        Channels
-      </Text>
-      <Fade in>
+    <Fade in>
+      <Box p={4}>
+        <Text fontSize="lg" fontWeight="medium">
+          Channels
+        </Text>
         <Box my={4}>
           <LinkBox py={3} px={4} borderWidth="0.5px" borderRadius="base">
             <Text fontWeight="medium">Public</Text>
           </LinkBox>
         </Box>
-      </Fade>
-    </Box>
+
+        <Flex flexDir="column" my={4}>
+          <Tooltip label="Hey! Wilson is still working on this feature...">
+            <Button borderWidth="0.5px" borderRadius="base">
+              <Text fontWeight="medium">Create a new room</Text>
+            </Button>
+          </Tooltip>
+        </Flex>
+      </Box>
+    </Fade>
   );
 };
 
