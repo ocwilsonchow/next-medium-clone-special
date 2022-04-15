5e821bd640fa9347e56c791e737a66b4902b33af
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-15 20:04:48 +0800

    removed sanity adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,17 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { SanityAdapter, SanityCredentials } from "next-auth-sanity";
+import GitHubProvider from "next-auth/providers/github";
 
-import { writeClient } from "../../../lib/sanity";
-
-const options = {
+export default NextAuth({
   providers: [
-    GithubProvider({
+    // OAuth authentication providers...
+    GitHubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
     }),
-
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
     }),
-    SanityCredentials(writeClient),
   ],
-  secret: "any-secret-word",
-  session: {
-    strategy: 'jwt'
-  },
-  adapter: SanityAdapter(writeClient),
-};
-
-export default NextAuth(options);
+});
