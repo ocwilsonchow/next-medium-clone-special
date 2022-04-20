53f62034c778b6442c45d2041ebc7ac2f08570c3
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-20 20:04:26 +0800

    update

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,8 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
-import { useChat } from "../../context/ChatContext";
 import { useSession, signIn, signOut } from "next-auth/react";
-import Auth from "../../components/chatEngine/Auth";
-import Authenticated from "../../components/chatEngine/Authenticated"
 
 const PageChatEngine = () => {
   const { data: session } = useSession();
