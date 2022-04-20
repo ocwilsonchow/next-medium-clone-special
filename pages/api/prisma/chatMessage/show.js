fa64839e0d43d46225aa4cfd415d0c42fd500891
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-20 20:00:15 +0800

    update

--- pages/api/prisma/chatMessage/show.js
+++ pages/api/prisma/chatMessage/show.js
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
-  const chatrooms = await prisma.chatMessage.findMany({
-    where: {
-      chatRoomId:
-    }
-  });
+// export default async function handler(req, res) {
+//   const chatrooms = await prisma.chatMessage.findMany({
+//     where: {
+//       chatRoomId:
+//     }
+//   });
 
-  res.status(200).json(chatrooms);
-}
+//   res.status(200).json(chatrooms);
+// }
