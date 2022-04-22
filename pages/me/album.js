fcbb5dd56909c671cb2d74ed852a12e4682cb963
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-23 01:52:42 +0800

    update

--- pages/me/album.js
+++ pages/me/album.js
@@ -7,19 +7,13 @@ import {
   useColorModeValue,
   Img,
   Button,
+  Tooltip,
 } from "@chakra-ui/react";
 import { apiGetGalleryImages } from "../../lib/gallery";
 import imageUrlBuilder from "@sanity/image-url";
 
 const PageAlbum = ({ images }) => {
-  const [width, setWidth] = useState(0);
-  const carousel = useRef();
-  const boxColor = useColorModeValue("gray.50", 'gray.900')
-
-
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-  }, []);
+  const boxColor = useColorModeValue("gray.50", "gray.900");
 
   const builder = imageUrlBuilder({
     projectId: "zmau43jq",
@@ -36,42 +30,31 @@ const PageAlbum = ({ images }) => {
           My Album
         </Text>
       </Box>
-      <motion.div
-        ref={carousel}
-        className="carousel cursor-grab overflow-hidden pb-2"
-        whileTap={{ cursor: "grabbing" }}
-      >
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-carousel flex"
-        >
-          {images.map((img, i) => (
-            <motion.div key={i} className="mr-1 p-1">
-              <Flex
-                flexDir="column"
-                p={6}
-                borderWidth="0.5px"
-                w={["300px","350px","400px","400px"]}
-                borderRadius="base"
-                justifyContent="space-between"
-                bgColor={boxColor}
-              >
+
+      <Flex flexWrap="wrap">
+        {images.map((img, i) => (
+          <motion.div key={i} className="mr-1 p-1">
+            <Flex
+              flexDir="column"
+              p={6}
+              borderWidth="0.5px"
+              borderRadius="base"
+              justifyContent="space-between"
+              bgColor={boxColor}
+            >
+              <Tooltip fontSize="md" label={img.description}>
                 <Img
                   borderRadius="base"
                   src={urlFor(img?.mainImage)}
-                  width="full"
-                  h={["350px","400px","500px","500px"]}
+                  boxSize="250px"
                   objectFit="cover"
                   draggable="false"
                 />
-                <Text pt={4} fontWeight="bold">{img.title}</Text>
-                <Text py={4}>{img.description}</Text>
-              </Flex>
-            </motion.div>
-          ))}
-        </motion.div>
-      </motion.div>
+              </Tooltip>
+            </Flex>
+          </motion.div>
+        ))}
+      </Flex>
     </>
   );
 };
