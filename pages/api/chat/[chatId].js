c719e8a84bc80cd8f378a97f13de1b2b66f9fe01
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-04-28 22:05:17 +0800

    update

--- pages/api/chat/[chatId].js
+++ pages/api/chat/[chatId].js
@@ -33,7 +33,6 @@ export default async function handler(req, res) {
               }
             },
           },
-          // users: true,
         },
       });
       res.status(200).json(foundChats);
