1fba7ea2681d7c61b3a2b497c7765ba007660872
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-05-20 11:15:08 +0800

    update project list

--- components/home/FeaturedProject.jsx
+++ components/home/FeaturedProject.jsx
@@ -1,16 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Box, Button, Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import Image from "next/image";
-import ecom from "../../images/react_ecom.png";
-import anothergram from "../../images/anothergram.png";
-import oxygen from "../../images/oxygen.png";
-import personal from "../../images/personal3.png";
-import uber from "../../images/uberclone.png";
-import okie from "../../images/okiee.png";
-import tie from "../../images/tie.png";
-import bni from "../../images/bni.png";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { projects } from "./projects"
 
 const FeaturedProject = () => {
   const carousel = useRef();
@@ -20,72 +13,7 @@ const FeaturedProject = () => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
   }, []);
 
-  const projects = [
-    {
-      title: "Doctor Booking App",
-      description:
-        "An UI-focused doctor booking system built with Next.js and Chakra UI. This project was completed in 12 hours. ",
-      image: tie,
-      link: "/",
-      remark: "PM for demo link",
-    },
-    {
-      title: " Next.js Blog",
-      description:
-        "A blog and portfolio website built with Next, NextAuth, Sanity, Supabase, SWR, Prisma, Framer Motion, and Chakra UI.",
-      image: personal,
-      link: "/",
-      remark: "See it on Vercel",
-    },
 
-    {
-      title: " React E-Commerce",
-      description:
-        "An interactive online store built with React, Firestore, Firebase Authentication, Algolia, Stripe Checkout, PayPal and Chakra UI.",
-      image: ecom,
-      link: "https://react-ecommerce-app-tan.vercel.app",
-      remark: "See it on Vercel",
-    },
-
-    {
-      title: "Uber Blockchain Clone",
-      description:
-        "A Next.js clone of the Uber app, built with Mapbox, Ethers and Tailwind.",
-      image: uber,
-      link: "https://next-uber-clone-blockchain.vercel.app/",
-      remark: "See it on Vercel",
-    },
-    {
-      title: "Business Landing Page",
-      description: "A Next.js landing page for a business organization.",
-      image: bni,
-      link: "https://bni-faith.vercel.app",
-      remark: "See it on Vercel",
-    },
-    {
-      title: "Next.js Landing Page",
-      description: "A modern landing page built with Chakra UI. ",
-      image: okie,
-      link: "https://next-landing-page-sage-tau.vercel.app/",
-      remark: "See it on Vercel",
-    },
-    {
-      title: "Medicine Library",
-      description:
-        "A responsive Next.js static generation website built with Chakra UI and GraphCMS.",
-      image: oxygen,
-      link: "https://oxygen-pro.vercel.app/",
-      remark: "See it on Vercel",
-    },
-    {
-      title: "Full Stack Social Media",
-      description:
-        "A full stack CRUD project built with Node, Express, EJS, JQuery, Socket.io and Prisma. Features included: realtime chat, comment and post creation.",
-      image: anothergram,
-      link: "https://serene-garden-37851.herokuapp.com/",
-      remark: "See it on Heroku*",
-    },
-  ];
 
   return (
     <Box mb={20}>
---
+++ components/home/projects.js
@@ -0,0 +1,82 @@
+import ecom from "../../images/react_ecom.png";
+import anothergram from "../../images/anothergram.png";
+import oxygen from "../../images/oxygen.png";
+import personal from "../../images/personal3.png";
+import uber from "../../images/uberclone.png";
+import okie from "../../images/okiee.png";
+import tie from "../../images/tie.png";
+import bni from "../../images/bni.png";
+import todo from "../../images/todo.png";
+
+export const projects = [
+  {
+    title: " React E-Commerce",
+    description:
+      "An interactive online store built with React, Firestore, Firebase Authentication, Algolia, Stripe Checkout, PayPal and Chakra UI.",
+    image: ecom,
+    link: "https://react-ecommerce-app-tan.vercel.app",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Doctor Booking App",
+    description:
+      "An UI-focused doctor booking system built with Next.js and Chakra UI. This project was completed in 12 hours. ",
+    image: tie,
+    link: "/",
+    remark: "PM for demo link",
+  },
+  {
+    title: " Next.js Blog",
+    description:
+      "A blog and portfolio website built with Next, NextAuth, Sanity, Supabase, SWR, Prisma, Framer Motion, and Chakra UI.",
+    image: personal,
+    link: "/",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Next.s Sanity To-do",
+    description:
+      "A quick to-do app built with Next.js, Sanity CMS and Chakra UI, with customized Chakra theme.",
+    image: todo,
+    link: "https://next-sanity-todo.vercel.app/",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Uber Blockchain Clone",
+    description:
+      "A Next.js clone of the Uber app, built with Mapbox, Ethers and Tailwind.",
+    image: uber,
+    link: "https://next-uber-clone-blockchain.vercel.app/",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Business Landing Page",
+    description: "A Next.js landing page for a business organization.",
+    image: bni,
+    link: "https://bni-faith.vercel.app",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Next.js Landing Page",
+    description: "A modern landing page built with Chakra UI. ",
+    image: okie,
+    link: "https://next-landing-page-sage-tau.vercel.app/",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Medicine Library",
+    description:
+      "A responsive Next.js static generation website built with Chakra UI and GraphCMS.",
+    image: oxygen,
+    link: "https://oxygen-pro.vercel.app/",
+    remark: "See it on Vercel",
+  },
+  {
+    title: "Full Stack Social Media",
+    description:
+      "A full stack CRUD project built with Node, Express, EJS, JQuery, Socket.io and Prisma. Features included: realtime chat, comment and post creation.",
+    image: anothergram,
+    link: "https://serene-garden-37851.herokuapp.com/",
+    remark: "See it on Heroku*",
+  },
+];
